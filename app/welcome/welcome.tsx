4b21ad3a4ef2ea891c5101356a3b62990daa6139
saraRekab <s.rekabtalaei@gmail.com>
2025-01-08 12:42:00 -0500

    WIP

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -69,7 +69,7 @@ const resources = [
   },
   {
     href: "https://rmx.as/discord",
-    text: "Join Discord",
+    text: "Join Sara and Hadis",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
